2fa938cb265aaf4c485f8c68174243c3ace06d01
Kabelo Mamadi <kabelo@chronologic.co.za>
2021-10-14 22:30:44 +0200

    Restructured code

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -33,31 +33,10 @@ class MembersController < ApplicationController
 
   def log_match
     if params[:match].present?
-      first_member = Member.find(match_params[:first_member_id])
-      second_member = Member.find(match_params[:second_member_id])
-      winning_member = match_params[:winning_member_id]
-      Match.create!(first_member_id: first_member.id, second_member_id: second_member.id,
-                    winning_member_id: winning_member)
-      first_member.games_played += 1
-      second_member.games_played += 1
-      case winning_member
-      when '1'
-        calculate_rank(first_member, second_member)
-      when '2'
-        calculate_rank(second_member, first_member)
-      when '3'
-        if (first_member.rank - second_member.rank).abs > 1
-          if first_member.rank > second_member.rank
-            first_member.rank -= 1
-            Member.where(rank: first_member.rank).update(rank: first_member.rank + 1)
-          else
-            second_member.rank -= 1
-            Member.where(rank: second_member.rank).update(rank: second_member.rank + 1)
-          end
-        end
-      first_member.save!
-      second_member.save!
-      end
+      match = Match.create!(first_member_id: match_params[:first_member_id],
+                            second_member_id: match_params[:second_member_id],
+                            winning_member_id: match_params[:winning_member_id])
+      match.log_result
       redirect_to members_view_all_path
     end
   end
@@ -66,26 +45,13 @@ class MembersController < ApplicationController
     @members = Member.where(active: true).order(rank: :asc)
   end
 
+  private
+
   def member_params
     params.require(:member).permit(:id, :name, :surname, :email, :birthday)
   end
 
   def match_params
     params.require(:match).permit(:first_member_id, :second_member_id, :winning_member_id)
-  end
-
-  private
-
-  def calculate_rank(winner, loser)
-    if winner.rank > loser.rank
-      lower_rank = winner.rank
-      higher_rank = loser.rank
-      winner.rank = lower_rank - ((lower_rank - higher_rank) / 2)
-      Member.where('rank >= ?', winner.rank).update_all('rank = rank + 1')
-      loser.rank = higher_rank + 1
-      Member.where(rank: loser.rank).update(rank: loser.rank - 1)
-    end
-    winner.save!
-    loser.save!
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,43 @@
 class Match < ActiveRecord::Base
+
+  # Update the rank of the members who have played based on result
+  def log_result
+    first_member = Member.find(first_member_id)
+    second_member = Member.find(second_member_id)
+    winning_member = winning_member_id
+    first_member.games_played += 1
+    second_member.games_played += 1
+    case winning_member
+    when 1
+      calculate_rank(first_member, second_member)
+    when 2
+      calculate_rank(second_member, first_member)
+    when 3
+      if (first_member.rank - second_member.rank).abs > 1
+        if first_member.rank > second_member.rank
+          first_member.rank -= 1
+          Member.where(rank: first_member.rank).update(rank: first_member.rank + 1)
+        else
+          second_member.rank -= 1
+          Member.where(rank: second_member.rank).update(rank: second_member.rank + 1)
+        end
+      end
+      first_member.save!
+      second_member.save!
+    end
+  end
+
+  # Update the leader board to factor in the latest result and new ranks
+  def calculate_rank(winner, loser)
+    if winner.rank > loser.rank
+      lower_rank = winner.rank
+      higher_rank = loser.rank
+      winner.rank = lower_rank - ((lower_rank - higher_rank) / 2)
+      Member.where('rank >= ?', winner.rank).update_all('rank = rank + 1')
+      loser.rank = higher_rank + 1
+      Member.where(rank: loser.rank).update(rank: loser.rank - 1)
+    end
+    winner.save!
+    loser.save!
+  end
 end
