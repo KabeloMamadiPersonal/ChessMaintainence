116eb1bc1a987401d74292392041f6fb19598362
Kabelo Mamadi <kabelo@chronologic.co.za>
2021-10-15 14:38:53 +0200

    Added datepicker in the creation of members

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-Member.find_or_create_by!(name: 'Jane', surname: 'Doe', email: 'Janedoe@gmail.com', birthday: '01/01/1991',
+Member.find_or_create_by!(name: 'Jane', surname: 'Doe', email: 'Janedoe@gmail.com', birthday: '1991-01-01',
                           rank: Member.where(active: true).count + 1)
-Member.find_or_create_by!(name: 'Peter', surname: 'Drury', email: 'PeterDrury@gmail.com', birthday: '01/02/1991',
+Member.find_or_create_by!(name: 'Peter', surname: 'Drury', email: 'PeterDrury@gmail.com', birthday: '1991-02-01',
                           rank: Member.where(active: true).count + 1)
-Member.find_or_create_by!(name: 'Mark', surname: 'Smith', email: 'MarkSmith@gmail.com', birthday: '01/03/1991',
+Member.find_or_create_by!(name: 'Mark', surname: 'Smith', email: 'MarkSmith@gmail.com', birthday: '1991-03-01',
                           rank: Member.where(active: true).count + 1)
-Member.find_or_create_by!(name: 'Will', surname: 'Fax', email: 'WillFax@gmail.com', birthday: '01/04/1991',
+Member.find_or_create_by!(name: 'Will', surname: 'Fax', email: 'WillFax@gmail.com', birthday: '1991-04-01',
                           rank: Member.where(active: true).count + 1)
