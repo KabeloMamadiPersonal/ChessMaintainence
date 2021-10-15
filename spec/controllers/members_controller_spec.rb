b057c180a0ed233c0bde948ace42c3fbdf46bd35
Kabelo Mamadi <kabelo@chronologic.co.za>
2021-10-15 09:34:09 +0200

    Two tests added

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,6 +1,4 @@
-require './app/controllers/members_controller'
 require 'rails_helper'
-# require Rails.root.join('db/migrate/20211013180101_create_member.rb')
 
 RSpec.describe MembersController, type: :controller do
   describe 'create member' do
@@ -12,10 +10,9 @@ RSpec.describe MembersController, type: :controller do
       post :create, params: { member: { name: test_name, surname: surname, birthday: birthday,
                                         email: email } }
       expect(Member.all.count).to eq 1
-      # expect(Member.first.name).to eq test_name
       expect(Member.first.surname).to eq surname
       expect(Member.first.email).to eq email
-      expect(Member.first.rank).to eq rank
+      expect(Member.first.rank).to eq 1
     end
   end
 end
